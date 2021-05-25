bf97ccdd1d0f77a7875758071be304b1bbc1e691
firstlinuxMint <firstlinuxmint@gmail.com>
2021-05-25 06:36:57 +0000

    Add document comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::path::PathBuf;
 use structopt::StructOpt;
+
+/// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
+    /// Patterns to search for
     pattern: String,
+    /// File path of the search object
     #[structopt(parse(from_os_str))]
     path: PathBuf,
 }
