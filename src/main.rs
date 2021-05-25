1faa3bdd3f1203b2a83559fb14b2f168cfe5eec6
firstlinuxMint <firstlinuxmint@gmail.com>
2021-05-25 06:30:54 +0000

    Use structopt to perform CLI argument processing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
+use structopt::StructOpt;
+#[derive(StructOpt)]
 struct Cli {
     pattern: String,
-    path: std::path::PathBuf,
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
 }
 
 fn main() {
-    let pattern = std::env::args().nth(1).expect("no pattern given");
-    let path = std::env::args().nth(2).expect("no path given");
-    let args = Cli {
-        pattern: pattern,
-        path: std::path::PathBuf::from(path),
-    };
+    let args = Cli::from_args();
 }
